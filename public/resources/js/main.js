3d520bb9feb6af931ed5aa9cbda58f6ae3f381d5
root <root@DESKTOP-3PBMK8U.localdomain>
2018-12-03 00:28:27 +0100

    Add third description par and link to about me section

--- public/resources/js/main.js
+++ public/resources/js/main.js
@@ -1,10 +1,10 @@
 var i = 0;
 var a = 0;
-//var b = 0;
+var b = 0;
 var txt_one = "HI, I'M GIULIO."; /* The text */
 var txt_two = "Don't mind me, I'm just a guy/astronaut who started a course on how to build a website from scratch and now he's trying to apply what he has learnt."; /* The text */
-// var txt_three = 'The answer my friend is blowing in the wind, or maybe is only one click from you, who knows...'; /* The text */
-var speed = 40; /* The speed/duration of the effect in milliseconds */
+var txt_three = "By the way, if you are interested in learning something more about me don't be afraid to scroll down!"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 document.addEventListener('DOMContentLoaded', typeWriter_one);
 
@@ -23,13 +23,15 @@ function typeWriter_two() {
     document.getElementById("par_two").innerHTML += txt_two.charAt(a);
     a++;
     setTimeout(typeWriter_two, speed);
+  }  else {
+    typeWriter_three();
   }
 }
 
-//function typeWriter_three() {
-//  if (b < txt_three.length) {
-//    document.getElementById("par_three").innerHTML += txt_three.charAt(b);
-//    b++;
-//    setTimeout(typeWriter_three, speed);
-//  }
-//}
+function typeWriter_three() {
+  if (b < txt_three.length) {
+    document.getElementById("par_three").innerHTML += txt_three.charAt(b);
+    b++;
+    setTimeout(typeWriter_three, speed);
+  }
+}
